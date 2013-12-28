eca903a94d97e27c5e40e76c5f2d3ca8f9212c96
Denis Brandolini <denis.brandolini@gmail.com>
2013-12-28 01:36:04 +0100

    Corrected the populator sh

--- populate.sh
+++ populate.sh
@@ -1,6 +1,6 @@
 # populate.sh file.txt [heroku]
-if [ "$2" == "heroku"]; then
-	addr="http://gospel99.herokuapp.com""
+if [ "$2" == "heroku" ]; then
+	addr="http://gospel99.herokuapp.com"
 else
 	addr="127.0.0.1:8088"
 fi
